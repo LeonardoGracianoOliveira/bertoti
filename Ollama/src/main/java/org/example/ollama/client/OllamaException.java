17e297c061f2f05e21d538ff5b2b6da60bb91215
Leonardo Graciano de Oliveira <leo.graciano.oliveira@gmail.com>
2025-10-17 19:18:58 -0300

    Delete Ollama directory

--- Ollama/src/main/java/org/example/ollama/client/OllamaException.java
+++
@@ -1,7 +0,0 @@
-package org.example.ollama.client;
-
-
-public class OllamaException extends Exception {
-    public OllamaException(String message) { super(message); }
-    public OllamaException(String message, Throwable cause) { super(message, cause); }
-}
